78a60a10b67bb0114df99595121ffd9e31f5e301
mhd6271 <maxmuehlbronner@gmail.com>
2026-09-06 12:53:59 +0200

    Update fiido_link.cpp
    UUID changed for Model Fiido AIR

--- components/fiido_bms/fiido_link.cpp
+++ components/fiido_bms/fiido_link.cpp
@@ -10,10 +10,10 @@ namespace esphome::fiido_bms {
 
 static const char *const TAG = "fiido_bms";
 
-// Custom Fiido service (FFE0 base): write to FFE2, notify from FFE1.
-static const auto SERVICE_UUID = esp32_ble_tracker::ESPBTUUID::from_raw("00010203-0405-0607-0809-0a0b0c0dffe0");
-static const auto NOTIFY_CHAR_UUID = esp32_ble_tracker::ESPBTUUID::from_raw("00010203-0405-0607-0809-0a0b0c0dffe1");
-static const auto WRITE_CHAR_UUID = esp32_ble_tracker::ESPBTUUID::from_raw("00010203-0405-0607-0809-0a0b0c0dffe2");
+// Custom Fiido service (FEA0 base, Fiido Air): write to FEA2, notify from FEA1.
+static const auto SERVICE_UUID = esp32_ble_tracker::ESPBTUUID::from_raw("c3e6fea0-e966-1000-8000-be99c223df6a");
+static const auto NOTIFY_CHAR_UUID = esp32_ble_tracker::ESPBTUUID::from_raw("c3e6fea1-e966-1000-8000-be99c223df6a");
+static const auto WRITE_CHAR_UUID = esp32_ble_tracker::ESPBTUUID::from_raw("c3e6fea2-e966-1000-8000-be99c223df6a");
 
 bool FiidoLink::resolve(ble_client::BLEClient *parent) {
   auto *notify_chr = parent->get_characteristic(SERVICE_UUID, NOTIFY_CHAR_UUID);
